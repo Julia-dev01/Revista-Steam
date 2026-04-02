cea16137a0a887a125f375c4b576a001dfd7aa31
Julia-dev01 <julia.paim@aluno.senai.br>
2026-04-02 19:32:01 -0300

    Add files via upload

---
+++ jp.js
@@ -0,0 +1,5 @@
+console.log("Olá, mundo! Este é meu primeiro código");
+console.log("Aluno: Julia das Neves");
+console.log("idade:17");
+console.log("expectativas: Manter a saúde mental");
+console.log("Aula01 - introdução ao Back-End");
